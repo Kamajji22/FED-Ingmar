025209354d71b15609e01a5c348b9d760cca0f03
Kamajji22 <ingmardijt@gmail.com>
2024-09-30 10:39:47 +0200

    fuckerdefuckerdefuck

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,6 @@ let openBurger = document.querySelector (".hamburgerKnop"); //vind de .hamburger
 let sluitBurger = document.querySelector ("#sluitHamburger"); //vind de #sluitHamburger
 
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const MaatOnderboek = document.querySelectorAll(".pagina2 main ul:nth-of-type(3) li"); //selecteerd de ul van de maten
-
-const winkelmand = document.querySelector("#inWinkelmand");
-const originalPosition = winkelmand.getBoundingClientRect().top + window.scrollY; // Verkrijg de originele positie van de winkelmand op basis van de horizontale scroll
 
 openBurger.onclick = openMenu; //roept de functie openMenu aan als je op openBurger (de hamburgermenuknop) drukt
 sluitBurger.onclick = sluitMenu; //roept de functie sluitMenu aan als je op sluitBurger (het kruisje) drukt
@@ -21,7 +17,9 @@ function sluitMenu() { //deze functie haalt de toonMenu id weg waardoor het hamb
   hamburgerMenu.removeAttribute("id");
 }
 
-//hamburgermenu met hulp van de turorial op DLO en een beetje chatgpt omdat ik een ID inplaats van een class gebruik
+//hamburgermenu met hulp van de turorial op DLO
+
+const MaatOnderboek = document.querySelectorAll(".pagina2 main ul:nth-of-type(3) li"); //selecteerd de ul van de maten
 
 //selecteerd de li's in de maat ul 
 MaatOnderboek.forEach(item => { 
@@ -40,14 +38,17 @@ MaatOnderboek.forEach(item => {
 
 //hulp van chatgpt 
 
+const inWinkelmand = document.querySelector("#inWinkelmand");
+const originalPosition = inWinkelmand.getBoundingClientRect().top + window.scrollY; // Verkrijg de originele positie van de winkelmand op basis van de horizontale scroll
+
 document.addEventListener("scroll", function() {
-    const currentScroll = window.scrollY; // scrollpositie van de window, horizontaal (y)
+    const currentScroll = window.scrollY; // scrollpositie van de window, verticaal (y)
 
     // als de huidige scollpositie groter is dan de originele positie van de winkelmand gaat de winkelmand bovenaanstaan
-    if (currentScroll > originalPosition + 350 ) { //de +340 pixels zorgt er voor dat hij niet meteen bovenaan gaat staan, maar pas 340 pixels later
-        winkelmand.classList.add("fixed-top"); // Voeg de fixed top class toe
+    if (currentScroll > originalPosition + 305 ) { //de +305 pixels zorgt er voor dat hij niet meteen bovenaan gaat staan, maar pas 340 pixels later
+        inWinkelmand.classList.add("fixed-top"); // Voeg de fixed top class toe
     } else {
-        winkelmand.classList.remove("fixed-top"); // Verwijdert de fixed top
+        inWinkelmand.classList.remove("fixed-top"); // Verwijdert de fixed top
     }
 });
 
