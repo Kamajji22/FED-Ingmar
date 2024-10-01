a895b9093e3a5d8ee20715cb6f425c6d93b5c4a4
Kamajji22 <ingmardijt@gmail.com>
2024-10-01 15:13:25 +0200

    2 Scripts

--- scripts/scripProduct.js
+++ scripts/scripProduct.js
@@ -1,22 +1,3 @@
-// JavaScript Document
-console.log("hi");
-
-let openBurger = document.querySelector (".hamburgerKnop"); //vind de .hamburgerknop
-let sluitBurger = document.querySelector ("#sluitHamburger"); //vind de #sluitHamburger
-
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
-
-openBurger.onclick = openMenu; //roept de functie openMenu aan als je op openBurger (de hamburgermenuknop) drukt
-sluitBurger.onclick = sluitMenu; //roept de functie sluitMenu aan als je op sluitBurger (het kruisje) drukt
-
-function openMenu() { //deze functie doet de toonMenu id aan waardoor de hamburgermenu tevoorschijn komt
-    hamburgerMenu.id = "toonMenu";
-  }
-
-function sluitMenu() { //deze functie haalt de toonMenu id weg waardoor het hamburgermenu weg gaat
-  hamburgerMenu.removeAttribute("id");
-}
-
 //hamburgermenu met hulp van de turorial op DLO
 
 const MaatOnderboek = document.querySelectorAll(".pagina2 main ul:nth-of-type(3) li"); //selecteerd de ul van de maten
